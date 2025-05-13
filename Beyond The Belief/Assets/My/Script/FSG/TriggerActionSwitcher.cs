b8571ec5c0f19492353fabb115dad49656ec23c8
Ju-Q <virgo1049479948@gmail.com>
2025-05-13 20:27:54 +0800

    AnimatorTriggerChange

---
+++ Beyond The Belief/Assets/My/Script/FSG/TriggerActionSwitcher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActionSwitcher: MonoBehaviour
+{
+    public string targetTag = "ActionTrigger";    // 触发区域的标签
+    public string animationTriggerName = "SitTrigger";  // Animator 中的触发器参数
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("未找到 Animator 组件！");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            animator.SetTrigger(animationTriggerName);
+            Debug.Log("触发了动作切换！");
+        }
+    }
+}
+
